a0e518d7d848a83103c43832fa0fb139ea7a79ef
MTeke1 <melihteke@gmail.com>
2021-10-17 23:03:23 +0200

    improved

--- text_processing/text_process.py
+++ text_processing/text_process.py
@@ -2,17 +2,20 @@
 
 import re
 
-pattern = 'router ospf'
+pattern = 'router ospf.*ip http server'
 
 f = open("switch_config.cfg", "r")
 
 text = f.read()
-result = re.search(pattern, text)
+result = re.search(pattern, text, re.DOTALL)
 s = result.start()
 e = result.end()
-print(s)
-print(e)
-print(result)
-#print(text[s:e])
+#print(s)
+#print(e)
+#print(result)
+print(text[s:e])
+
+
+print(text[s:e]) - 14     # removed "ip forward-protocol nd" command by deleting 14 characters
 
 f.close()
